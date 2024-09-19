cd2ea9c19d3f9a40478b63909597cea5a65df1dd
laura.vilchez <laura.vilchez@auleseebe.upc.edu>
2024-09-19 13:31:36 +0200

    personal

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,14 @@
 #Codigo para problema 2
+
+ls()
+#me dice las variables que he empleado para hacer el ejercicio
+
+rm(list = ls())
+#lo que hace lo de arriba es borra todo lo que tengo
+
+data <- mtcars
+data
+hist(data$mpg)
+
+#pie de numeros repetidos
+pie(table(data$cyl))
