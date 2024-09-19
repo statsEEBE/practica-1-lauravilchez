b5a83ab403d899f78260acb5f6951dff35a15cc0
laura.vilchez <laura.vilchez@auleseebe.upc.edu>
2024-09-19 13:43:00 +0200

    personal

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,24 @@
 #Codigo para problema 3
+
+x <- (1:30)/30
+#suma de todos los datos
+sum(x)
+#Para extraer una posicion del vector
+x[2]
+
+#Para extraer varias posiciones del vector
+x[c( 2, 4 ,6)]
+
+#Para eliminarlas
+x[-c( 2, 4 ,6)]
+
+#crea un vector y con unas poosiciones
+y <- x[c(21, 2, 26)]
+
+#suma de exponenciales x & y
+sum(exp(x))-sum(exp(y))
+
+y[19]
+y[24]
+
+
